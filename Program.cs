9025916f9c7cc4a016f249a80206a9158cacff89
churchrat <mihailkrustev08@gmail.com>
2024-12-01 21:43:42 +0200

    com

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ImplementStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack p = new Stack();
+            p.Push(10);
+            p.Push(20);
+            p.Push(30);
+            Console.WriteLine(p.Pop());
+            Console.WriteLine(p.Pop());
+            Console.WriteLine(p.Pop());
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementStack
+{
+    public class Stack
+    {
+        public static int count = 0;
+        private List<int> element = new List<int>();
+        public Stack()
+        {
+
+        }
+        public void Push(int item)
+        {
+            element.Add(item);
+            count++;
+        }
+        public int Pop()
+        {
+            int temp = element[element.Count - 1];
+            element.Remove(element[element.Count - 1]);
+            return temp;
+
+        }
+        public int Peek()
+        {
+            return element[element[element.Count - 1]];
+        }
+    }
+}
